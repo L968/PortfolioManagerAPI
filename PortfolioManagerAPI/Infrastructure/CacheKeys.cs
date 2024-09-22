5d8d8ed80dd07a68f28bbb7adffef0b1e1ad9ae4
Lucas Constantino <lucas.c968@gmail.com>
2024-09-22 16:12:50 -0300

    Alterando chave de cache

--- PortfolioManagerAPI/Infrastructure/CacheKeys.cs
+++ PortfolioManagerAPI/Infrastructure/CacheKeys.cs
@@ -2,6 +2,6 @@
 
 public static class CacheKeys
 {
-    public const string InvestmentProducts = "investment_products";
+    public const string InvestmentProducts = "InvestmentProducts";
     public static string InvestmentProductStatement(int userId) => $"User_{userId}_InvestmentProductStatements";
 }
